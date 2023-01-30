04ebd75892dfb406a34b702befba60474fa98169
Alex <Alex@HOME-PC>
2023-01-30 21:52:27 +0300

    Task14.3.3

---
+++ Task14.3.3/Task14.3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task14._3._3
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var contacts = from c in phoneBook orderby c.Name, c.LastName select c;
+
+            foreach (var cont in contacts)
+                Console.WriteLine(cont.Name + ", " + cont.LastName);
+            Console.ReadKey();
+        }
+    }
+}
